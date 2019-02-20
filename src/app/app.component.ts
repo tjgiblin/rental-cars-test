25693df8f52cc192f2e7177773c05fa38c58294c
Thomas.Giblin <Thomas.Giblin@fdmgroup.com>
2019-02-20 14:56:37 +0000

    default dates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,28 @@ export class AppComponent {
 
   constructor()
   {
+    var todaysDate = new Date();
+    
+    var pickUpDate = new Date();
+    pickUpDate.setDate(todaysDate.getDate() + 2);
+  
+    var dropOffDate = new Date();
+    dropOffDate.setDate(pickUpDate.getDate() + 3);
+
+    this.pickUpDate = pickUpDate;
+    this.dropOffDate = dropOffDate;
   }
 
   title = 'Rentalcars.com';
 
   pickUpLocation: string;
-  pickUpDate: Date = new Date();
+  pickUpDate: Date = this.pickUpDate;
   pickUpHour: string = "10";
   pickUpMins: string = "00";
 
   differentDropOffLocation: boolean = false;
   dropOffLocation: string;
-  dropOffDate: Date = new Date();
+  dropOffDate: Date = this.dropOffDate;
   dropOffHour: string = "10";
   dropOffMins: string = "00";
 
